d1cdbc1f7cbdc0c91e5fa67a7c751f601e4c829d
J0onYEong <cjy2134@naver.com>
2024-08-09 20:43:09 +0900

    [Feat] View구현

--- RxUIState/RxUIState/ViewController.swift
+++ RxUIState/RxUIState/ViewController.swift
@@ -9,11 +9,66 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // 🖥️ 텍스트필드
+    let textField: UITextField = {
+        let field = UITextField()
+        field.backgroundColor = .red.withAlphaComponent(0.3)
+        field.font = UIFont.systemFont(ofSize: 40)
+        return field
+    }()
+    // 🖥️ 저장한값 가져오기 버튼
+    let fetchButton: UIButton = {
+        let btn = UIButton()
+        btn.setTitle("저장한값 가져오기", for: .normal)
+        btn.setTitleColor(.black, for: .normal)
+        btn.backgroundColor = .blue.withAlphaComponent(0.3)
+        return btn
+    }()
+    // 🖥️ 값 저장하기 버튼
+    let saveButton: UIButton = {
+        let btn = UIButton()
+        btn.setTitle("값 저장하기", for: .normal)
+        btn.setTitleColor(.black, for: .normal)
+        btn.backgroundColor = .green.withAlphaComponent(0.3)
+        return btn
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 5
+        stackView.alignment = .center
+        [
+            fetchButton,
+            saveButton,
+        ].forEach {
+            stackView.addArrangedSubview($0)
+        }
+        
+        [
+            textField,
+            stackView
+        ].forEach {
+            view.addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        NSLayoutConstraint.activate([
+            textField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            textField.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            textField.leftAnchor.constraint(equalTo: view.leftAnchor),
+            textField.rightAnchor.constraint(equalTo: view.rightAnchor),
+            
+            stackView.topAnchor.constraint(equalTo: textField.bottomAnchor, constant: 20),
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+        ])
     }
-
-
 }
 
+@available(iOS 17.0, *)
+#Preview("Preview", traits: .defaultLayout) {
+    
+    ViewController()
+}
---
+++ RxUIState/RxUIState/ViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ViewModel.swift
+//  RxUIState
+//
+//  Created by choijunios on 8/9/24.
+//
+
+import Foundation
